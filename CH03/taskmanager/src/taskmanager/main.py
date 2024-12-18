232bf768d29b0f724aa55d2ba3bc5657dc1b7e80
Shahin ABDI <contact@shahinabdi.fr>
2024-12-18 17:15:20 +0100

    fix: StartTask - current_task added feat: StopTask - Calculate duration - Write task in dictionary - Write in final file

--- CH03/taskmanager/src/taskmanager/main.py
+++ CH03/taskmanager/src/taskmanager/main.py
@@ -5,7 +5,8 @@ from datetime import datetime
 tasks = {}
 current_task = None
 start_time = None
-Duration = None
+end_time = None
+duration = None
 
 # Load
 try:
@@ -31,11 +32,42 @@ while True:
             continue
         task_name = input("Enter task name: ")
         start_time = time.time()
+        current_task = task_name
         print(f"Task {task_name} started!")
 
         if task_name not in tasks:
             tasks[task_name] = {"total_time": 0, "sessions": []}
+    elif choice == "2":
+        if current_task == None:
+            print("Error: No task running!")
+            continue
 
+        end_time = time.time()
+
+        duration = end_time - start_time
+
+        tasks[current_task]["total_time"] += duration
+        tasks[current_task]["sessions"].append(
+            {
+                "start": datetime.fromtimestamp(start_time).strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                ),
+                "end": datetime.fromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S"),
+                "duration": duration,
+            }
+        )
+
+        try:
+            f = open("tasks.json", "w")
+            json.dump(tasks, f)
+            f.close()
+        except:
+            print("Error: Saving data was unsuccessful!")
+
+        print(f"Task '{current_task}' stopped! Duration: {duration:.2f} seconds")
+
+        current_task = None
+        start_time = None
     elif choice == "5":
         break
 # 2. Stop task
