32a02d085cc01a56d66683d94c6d1203ecf67f94
Shahin ABDI <contact@shahinabdi.fr>
2024-12-18 16:59:55 +0100

    feat: menu for app implemented

--- CH03/taskmanager/src/taskmanager/main.py
+++ CH03/taskmanager/src/taskmanager/main.py
@@ -15,15 +15,27 @@ try:
 except:
     tasks = {}
 
-# 1. Start task
-if current_task != None:
-    print("Error: One task already running!")
-task_name = input("Enter task name: ")
-start_time = time.time()
-print(f"Task {task_name} started!")
+while True:
+    # Menu
+    print("\n1. Start task")
+    print("2. Stop task")
+    print("3. View task")
+    print("4. Delete task")
+    print("5. Exit")
 
-if task_name not in tasks:
-    tasks[task_name] = {"total_time": 0, "sessions": []}
+    choice = input("Choose option: ")
+    if choice == "1":
+        # 1. Start task
+        if current_task != None:
+            print("Error: One task already running!")
+        task_name = input("Enter task name: ")
+        start_time = time.time()
+        print(f"Task {task_name} started!")
+
+        if task_name not in tasks:
+            tasks[task_name] = {"total_time": 0, "sessions": []}
+    elif choice == "5":
+        break
 # 2. Stop task
 # 3. View tasks
 # 4. Delete task
