a03de488de84e1fa5ad4d835af56eae1827aa7ac
Shahin ABDI <contact@shahinabdi.fr>
2024-12-18 16:48:04 +0100

    feat: start task implemented

--- CH03/taskmanager/src/taskmanager/main.py
+++ CH03/taskmanager/src/taskmanager/main.py
@@ -16,6 +16,14 @@ except:
     tasks = {}
 
 # 1. Start task
+if current_task != None:
+    print("Error: One task already running!")
+task_name = input("Enter task name: ")
+start_time = time.time()
+print(f"Task {task_name} started!")
+
+if task_name not in tasks:
+    tasks[task_name] = {"total_time": 0, "sessions": []}
 # 2. Stop task
 # 3. View tasks
 # 4. Delete task
