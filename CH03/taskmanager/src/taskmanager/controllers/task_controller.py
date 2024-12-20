380a976214e86ec4cadbf0bf399ad862559e09da
Shahin ABDI <contact@shahinabdi.fr>
2024-12-20 15:28:29 +0100

    feat: methods skeleton added

--- CH03/taskmanager/src/taskmanager/controllers/task_controller.py
+++ CH03/taskmanager/src/taskmanager/controllers/task_controller.py
@@ -6,3 +6,21 @@ class TaskController:
     def __init__(self, model: TaskModel, view: TaskView):
         self.model = model
         self.view = view
+
+    def run(self) -> None:
+        pass
+
+    def handle_start_task(self) -> None:
+        pass
+
+    def handle_stop_task(self) -> None:
+        pass
+
+    def handle_view_tasks(self) -> None:
+        pass
+
+    def handle_delete_task(self) -> None:
+        pass
+
+    def handle_exit(self) -> None:
+        pass
