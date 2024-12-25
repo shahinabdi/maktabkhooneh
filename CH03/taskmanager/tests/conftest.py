7681133a5f6eb50b6aa2e3f680ee6f53635f62ee
Shahin ABDI <contact@shahinabdi.fr>
2024-12-25 14:45:19 +0100

    feat: mock_stdin implemented

--- CH03/taskmanager/tests/conftest.py
+++ CH03/taskmanager/tests/conftest.py
@@ -17,6 +17,27 @@ def mock_stdout(monkeypatch):
 
 
 @pytest.fixture
+def mock_stdin(monkeypatch):
+    """Mock stdin for input"""
+
+    class MockInput:
+        def __init__(self):
+            self.responses = []
+
+        def __call__(self, prompt):
+            if not self.responses:
+                return "default"
+            return self.responses.pop(0)
+
+        def add_response(self, *responses):
+            self.responses.extend(responses)
+
+    mock = MockInput()
+    monkeypatch.setattr(builtins, "input", mock)
+    return mock
+
+
+@pytest.fixture
 def task_view():
     """TaskView instance"""
     return TaskView
